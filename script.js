c36c971cf9572761b005a78623c2eb0e2c216413
rogue <fkclfm@naver.com>
2024-04-26 19:11:39 +0900

    로컬스토리지 작업

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const todoInput = document.querySelector('#todo-input');
+//내부로 들어가게 되면 함수 내부에서만 동작하는 변수가 되어
+// save함수에서 못쓰게된다.
+const todoList = document.querySelector('#todo-list');
 
 const createTodo = function () {
-  const todoList = document.querySelector('#todo-list');
   const newLi = document.createElement('li');
   const newSpan = document.createElement('span');
   const newBtn = document.createElement('button');
@@ -18,7 +20,8 @@ const createTodo = function () {
   newLi.appendChild(newBtn);
   todoList.appendChild(newLi);
   newLi.appendChild(newSpan);
-  todoInput.value = '';  
+  todoInput.value = '';
+  saveItemsFn();
 };
 const keyCodeCheck = function () {
   if(window.event.keyCode === 13 && todoInput.value !== '') {
@@ -32,4 +35,18 @@ const deleteAll = function () {
    for(let i = 0; i < liList.length; i++) {
     liList[i].remove();
    }
+};
+
+//로컬 스토리지로 to-do list 저장
+const saveItemsFn = function () {
+  const saveItems = [];
+  for(let i = 0; i < todoList.children.length; i++) {
+    //취소선까지 저장 위해 객체 작성
+    const todoObj = {
+      contents : todoList.children[i].querySelector('span').textContent,
+      complete : todoList.children[i].classList.contains('complete')
+    };
+    saveItems.push(todoObj);
+  }
+  console.log(saveItems);
 }
